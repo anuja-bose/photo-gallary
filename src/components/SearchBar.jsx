cc630a119d3a2124074b7a92f48cf9fd65b6a0d4
anuja <anuja@anujas-MacBook-Air.local>
2022-12-28 10:49:53 +0530

    added close button for the searchinput

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -13,7 +13,7 @@ const SearchBar = () => {
     setSearchKeyword(searchValue);
     //setSearchValue("");
     console.log("before navigate");
-    window.location.replace("/photos/search/"+searchValue);
+    window.location.replace("/photos/search/" + searchValue);
     console.log("after navigate");
   }
   const handleEnterSearch = (e) => {
@@ -21,39 +21,27 @@ const SearchBar = () => {
       e.preventDefault();
       setSearchValue("");
       setSearchKeyword(searchValue);
-      window.location.replace("/photos/search/"+searchValue);
+      window.location.replace("/photos/search/" + searchValue);
     }
   }
 
   return (
     <div className="flex">
-      <form className="form-inline">
-        <div className="input-group">
-          <input type="text" className="form-control search"
-            placeholder="Search free high-resolution photos"
-            aria-label="Username" value={searchValue}
-            onChange={handleInputChange}
-            onKeyDown={handleEnterSearch}
-            aria-describedby="basic-addon1"
-            />
+      <div className="input-group">
+        <div className="form-outline">
+          <input type="search" id="form1" className="form-control search" 
+          placeholder="Search free high-resolution photos"
+          onChange={handleInputChange}
+          onKeyDown={handleEnterSearch}/>
 
-          <div className="input-group-prepend">
-            <span id="basic-addon1">
-              <Button
-                onClick={handleButtonSearch}
-                disabled={!searchValue}
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                </svg></Button>
-
-            </span>
-          </div>
         </div>
-      </form>
-
-
-
+        <button type="button" className="btn btn-primary"  onClick={handleButtonSearch}
+                disabled={!searchValue}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+        </button>
+      </div>
     </div>
   )
 }
