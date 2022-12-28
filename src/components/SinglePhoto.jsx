352db6c442864cb47f1636a629733bfb2dca8708
anuja <anuja@anujas-MacBook-Air.local>
2022-12-28 15:23:00 +0530

    unlikes class added

--- src/components/SinglePhoto.jsx
+++ src/components/SinglePhoto.jsx
@@ -9,20 +9,22 @@ const SinglePhoto = ({ data }) => {
   // )
 
   return (
-    <Card style={{ width: '18rem', display :'inline-block', margin :'10px' }}>
+    <Card style={{ width: '18rem', display: 'inline-block', margin: '10px' }}>
       <Card.Img variant="top" src={data.urls.small} />
       <Card.Body>
-        
+
         <Card.Text>
-          <img width={40} height={40} className="img-thumbnail rounded object-cover" src={data.user.profile_image.medium} alt="user uploaded"/>
+          <img width={40} height={40} className="img-thumbnail rounded object-cover" src={data.user.profile_image.medium} alt="user uploaded" />
           <span className='mlr-5'>{data.user.name}</span>
-          <span className='likes'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-</svg></span>{data.likes}
-        
+          <span className={data.likes===0? "nolikes" : "likes"}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+            </svg>
+          </span>
+          {data.likes}
+
         </Card.Text>
-       
+
       </Card.Body>
     </Card>
   )
